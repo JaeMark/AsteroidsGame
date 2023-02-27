48382833c305dcfc9ebd1455ecc8061103172446
Jetro De Leon <35773004+JaeMark@users.noreply.github.com>
2023-02-27 16:56:58 -0600

    Added Asteroid class which is a child of the Actor class.

---
+++ Asteroids/asteroid.js
@@ -0,0 +1,5 @@
+class Asteroid extends Actor {
+  constructor(startingPosition, startingVelocity, size, sprite, health) {
+    super(startingPosition, startingVelocity, size, sprite, health);
+  }
+}
