05b0bc9eedf9dfd8e2b423713feb651fc9eeb874
Jetro De Leon <35773004+JaeMark@users.noreply.github.com>
2023-02-27 16:15:31 -0600

    Player ship now moves towards its heading direction when the thrust input is pressed.

--- Asteroids/character.js
+++ Asteroids/character.js
@@ -1,34 +1,32 @@
 class Character extends Actor {
-	constructor(startingPosition, startingVelocity, heading, size, sprite, health) {
-		super(startingPosition, startingVelocity, size, sprite, health);
-		this.heading = heading;
-		this.rotation = 0;
-		this.maxSpeed = createVector(10, 10);
-		this.acc = createVector(1, 1);
-		this.isThrusting = false;
-	}
-	
-	thrust() {
-		this.isThrusting = true;
-		//this.acc = createVector(10, 10);
-	}
-	
-	update() {
-		super.update();
-		// this.velocity.add(this.acc);
-		if(this.isThrusting) {
-			this.velocity.add(this.acc);
-			if(this.velocity == this.maxSpeed) {
-				this.isThrusting = false;
-				this.velocity = createVector(0, 0);
-			}
-		}
-	}
+  constructor(
+    startingPosition,
+    startingVelocity,
+    heading,
+    size,
+    sprite,
+    health
+  ) {
+    super(startingPosition, startingVelocity, size, sprite, health);
+    this.heading = heading;
+    this.rotation = 0;
+    this.maxSpeed = createVector(10, 10);
+    this.acc = createVector(1, 1);
+  }
 
-	display() {
-		push();
-			translate(this.position.x, this.position.y);
-			/*
+  thrust() {
+    let force = p5.Vector.fromAngle(this.heading);
+    this.velocity.add(force);
+  }
+
+  update() {
+    super.update();
+  }
+
+  display() {
+    push();
+    translate(this.position.x, this.position.y);
+    /*
 			// direction of the velocity.
 			if (keyIsPressed) {
 				let noseLength = map(this.velocity.mag(), 0, 10, 0, this.size * 2);
@@ -36,17 +34,15 @@ class Character extends Actor {
 				line(0, 0, direction.x, direction.y);
 			}
 			*/
-			this.heading += this.rotation;
-			rotate(this.heading);
-			super.display();
-		pop();
-	}
-	
-	setRotation(angle) {
-		this.rotation = angle;
-	}
+    this.heading += this.rotation;
+    rotate(this.heading + PI/2);
+    super.display();
+    pop();
+  }
 
-	shoot() {
-		
-	}
+  setRotation(angle) {
+    this.rotation = angle;
+  }
+
+  shoot() {}
 }
