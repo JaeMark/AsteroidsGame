639bdc5fcf8bf143186601a0422385b67df44ffd
Jetro De Leon <35773004+JaeMark@users.noreply.github.com>
2023-02-27 14:31:35 -0600

    Fixed space formatting.

--- Asteroids/actor.js
+++ Asteroids/actor.js
@@ -1,38 +1,37 @@
-class Actor{
-    constructor(startingPosition, startingVelocity, size, sprite, health) {
-      this.position = startingPosition;
-      this.velocity = startingVelocity;
-      this.size = size;
-      this.sprite = sprite;
-      this.health = health;
+class Actor {
+  constructor(startingPosition, startingVelocity, size, sprite, health) {
+    this.position = startingPosition;
+    this.velocity = startingVelocity;
+    this.size = size;
+    this.sprite = sprite;
+    this.health = health;
+  }
+
+  update() {
+    this.position.add(this.velocity);
+    this.checkEdges();
+  }
+
+  isDead() {
+    return this.health <= 0;
+  }
+
+  checkEdges() {
+    let collisionRadius = this.size / 2;
+    if (this.position.x > width - collisionRadius) {
+      this.position.x = 0;
+    } else if (this.position.x < collisionRadius) {
+      this.position.x = width;
     }
-  
-    update() {
-      this.position.add(this.velocity);
-      this.checkEdges();
-   
-    }
-    
-    isDead() {
-      return this.health <= 0;
-    }
-    
-    checkEdges() {
-      let collisionRadius = this.size / 2;
-      if (this.position.x > (width - collisionRadius)) {
-        this.position.x = 0;
-      } else if (this.position.x < collisionRadius) {
-        this.position.x = width;
-      }
-      
-      if (this.position.y > (height - collisionRadius)) {
-        this.position.y = 0;
-      } else if (this.position.y < collisionRadius) {
-        this.position.y = height;
-      }
-    }
-    
-    display() {
-      circle(this.position.x, this.position.y, 10);
+
+    if (this.position.y > height - collisionRadius) {
+      this.position.y = 0;
+    } else if (this.position.y < collisionRadius) {
+      this.position.y = height;
     }
   }
+
+  display() {
+    circle(this.position.x, this.position.y, 10);
+  }
+}
--- Asteroids/sketch.js
+++ Asteroids/sketch.js
@@ -2,14 +2,14 @@ let ship;
 
 function setup() {
   createCanvas(400, 400);
-  
+
   let startingPosition = createVector(width/2, height/2);
   let startingVelocity = createVector(0, 0);
   let spriteSize = 5;
   let sprite = 5;
   let health = 5;
   ship = new Character(startingPosition, startingVelocity, spriteSize, sprite, health);
-  
+
 }
 
 function draw() {
