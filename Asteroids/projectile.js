4e5aad41435fbce47c99626dc463a36daa77a2db
Jetro De Leon <35773004+JaeMark@users.noreply.github.com>
2023-03-08 16:13:35 -0600

    Projectiles now disappear after 2 secs.

--- Asteroids/projectile.js
+++ Asteroids/projectile.js
@@ -1,14 +1,23 @@
 class Projectile extends Actor {
   constructor(startingPosition, startingVelocity, radius, sprite) {
     super(startingPosition, startingVelocity, radius, sprite);
+    this.timeToLive = 2 * getFrameRate();
+    this.birthFrame = frameCount;
+  }
+
+  update() {
+    if (frameCount - this.birthFrame >= this.timeToLive) {
+      this.destoryProjectile();
+    }
+    super.update();
   }
 
   display() {
     push();
-      circle(this.position.x, this.position.y, this.radius);
+    circle(this.position.x, this.position.y, this.radius);
     pop();
   }
-  
+
   destoryProjectile() {
     this.position = createVector(-100, -100);
     this.velocity = createVector(0, 0);
