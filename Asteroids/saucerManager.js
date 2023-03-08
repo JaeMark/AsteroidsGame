8aba6c3b2b7c9f56cc2f2dc99236c9b8382d7187
Jetro De Leon <35773004+JaeMark@users.noreply.github.com>
2023-03-08 17:07:45 -0600

    Changed saucer spawn location

--- Asteroids/saucerManager.js
+++ Asteroids/saucerManager.js
@@ -1,7 +1,7 @@
 const SaucerSize = {
-	Large: 50,
-	Small: 40
-}
+  Large: 50,
+  Small: 40,
+};
 
 class SaucerManager {
   constructor(largeSprite, smallSprite, smallSaucerSpawnRate) {
@@ -10,44 +10,52 @@ class SaucerManager {
     this.smallSaucerSpawnRate = smallSaucerSpawnRate;
     this.saucers = [];
   }
-  
+
   spawnSaucer() {
     let size = SaucerSize.Large;
     let randomNum = random();
-    if(randomNum < this.smallSaucerSpawnRate) {
+    if (randomNum < this.smallSaucerSpawnRate) {
       size = SaucerSize.Small;
     }
-    
-    let spawnLocation = createVector(random(width), random(height));
-    
+
+    // spawn at the boundary
+    let spawnLocation = createVector(
+      floor(random(2) * width, floor(random(2) * height))
+    );
+
     switch (size) {
       case SaucerSize.Large:
-         this.saucers.push(new Saucer(
-                      spawnLocation, 
-                      p5.Vector.random2D(),
-                      size,
-                      this.largeSprite));
+        this.saucers.push(
+          new Saucer(
+            spawnLocation,
+            p5.Vector.random2D(),
+            size,
+            this.largeSprite
+          )
+        );
         break;
       case SaucerSize.Small:
-         this.saucers.push(new Saucer(
-                      spawnLocation, 
-                      p5.Vector.random2D(),
-                      size,
-                      this.smallSprite));
-      break;
+        this.saucers.push(
+          new Saucer(
+            spawnLocation,
+            p5.Vector.random2D(),
+            size,
+            this.smallSprite
+          )
+        );
+        break;
     }
   }
-  
+
   display() {
     for (let i = 0; i < this.saucers.length; i++) {
-        this.saucers[i].display();
+      this.saucers[i].display();
     }
   }
-  
+
   update() {
     for (let i = 0; i < this.saucers.length; i++) {
-        this.saucers[i].update();
+      this.saucers[i].update();
     }
   }
 }
-                    
