1c2c393e6da5414d29cde175f27e4bdb6a1e4603
Jetro De Leon <35773004+JaeMark@users.noreply.github.com>
2023-03-10 17:32:04 -0600

    The player can now restart the game.

--- Asteroids/sketch.js
+++ Asteroids/sketch.js
@@ -16,6 +16,9 @@ let smallSprite;
 let enemySpriteSmall;
 let enemySpriteLarge;
 
+let startButton;
+let restartButton;
+
 function preload() {
   playerSprite = loadImage("assets/PlayerShip.png");
   explosionSprite = loadImage("assets/Explosion.png");
@@ -23,14 +26,14 @@ function preload() {
   mediumSprite = loadImage("assets/AsteroidMedium.png");
   smallSprite = loadImage("assets/AsteroidSmall.png");
   enemySpriteSmall = loadImage("assets/EnemyShipSmall.png");
-  enemySpriteLarge =  loadImage("assets/EnemyShipLarge.png");
+  enemySpriteLarge = loadImage("assets/EnemyShipLarge.png");
 }
 
 const GameState = {
-	Start: "start",
-	Playing: "playing",
-	GameOver: "gameover"
-}
+  Start: "start",
+  Playing: "playing",
+  GameOver: "gameover",
+};
 let gameState = GameState.Start;
 
 function setup() {
@@ -42,51 +45,33 @@ function setup() {
   textStyle(BOLD);
   fill(255);
   textSize(20);
-  
+
   // Setup the start button
   textAlign(CENTER);
   startButton = createButton("Start Game");
   startButton.size(200, 75);
-  startButton.position(width/2-100, height/2-37.5);
+  startButton.position(width / 2 - 100, height / 2 - 37.5);
   startButton.mousePressed(startGame);
-  
-  let startingPosition = createVector(width / 2, height / 2);
-  let startingVelocity = createVector(0, 0);
-  let heading = 0;
-  let spriteSize = 20;
-  let health = 5;
-  ship = new Player(
-    startingPosition,
-    startingVelocity,
-    heading,
-    spriteSize,
-    playerSprite,
-    health
-  );
-  
-  asteroidManager = new AsteroidManager(
-    numAsteroids,
-    largeSprite,
-    mediumSprite,
-    smallSprite
-  );
-  
-  saucerManager = new SaucerManager(
-    enemySpriteLarge,
-    enemySpriteSmall,
-    smallSaucerSpawnProb
-  );
+
+  // Setup the restart button
+  restartButton = createButton("Restart Game");
+  restartButton.size(200, 75);
+  restartButton.position(width / 2 - 100, height / 2 - 37.5);
+  restartButton.mousePressed(restartGame);
+  restartButton.hide();
+
+  newGame();
 }
 
 function draw() {
   background(220);
 
   switch (gameState) {
-    case GameState.Start:  
+    case GameState.Start:
       push();
-        textAlign(CENTER);
-        textSize(100);
-        text("Asteroids", width/2, 225);
+      textAlign(CENTER);
+      textSize(100);
+      text("Asteroids", width / 2, 225);
       pop();
       break;
     case GameState.Playing:
@@ -94,53 +79,56 @@ function draw() {
       break;
     case GameState.GameOver:
       push();
-        ship.display();
-        image(explosionSprite, ship.position.x, ship.position.y, 100, 100);
-        asteroidManager.display();
-        background(220, 220, 220, 200);
-        translate(width/2, height/2);
-        textAlign(CENTER);
-        fill(255, 255, 255, 90);
-        rect(0, -15, 300, 125);
-        fill(0);
-        textSize(40);
-        text("GAME OVER!", 0, 0);
+      ship.display();
+      image(explosionSprite, ship.position.x, ship.position.y, 100, 100);
+      asteroidManager.display();
+      background(220, 220, 220, 200);
+      textAlign(CENTER);
+      textSize(100);
+      text("Game Over!", width / 2, 225);
+      restartButton.show();
       pop();
       break;
     default:
       console.log("Game mode: " + gameState + " not defined");
-  } 
-  
+  }
+
   displayScore();
   displayHealth();
-  
 }
 
 // Starts the game
 function startGame() {
   gameState = GameState.Playing;
   startButton.hide();
-}  
+}
+
+// Starts the game
+function restartGame() {
+  newGame();
+  gameState = GameState.Playing;
+  restartButton.hide();
+}
 
 function playingGameStateUpdate() {
-  if(ship.isDead()) {
+  if (ship.isDead()) {
     gameState = GameState.GameOver;
     return;
   }
-  
+
   let asteroids = asteroidManager.asteroids;
   let saucers = saucerManager.saucers;
 
   ship.checkProjectileCollision(asteroidManager, saucerManager);
   ship.displayProjectile();
   ship.updateProjectile();
-  
+
   ship.checkCollisions(asteroids);
   ship.checkCollisions(saucers);
   ship.display();
   ship.update();
-  
-  if(ship.score > nextSaucerSpawnInterval) {
+
+  if (ship.score > nextSaucerSpawnInterval) {
     nextSaucerSpawnInterval += saucerSpawnInterval;
     saucerManager.spawnSaucer();
   }
@@ -148,11 +136,11 @@ function playingGameStateUpdate() {
   saucerManager.checkProjectileCollision(asteroidManager, ship);
   saucerManager.displayProjectile();
   saucerManager.updateProjectile();
-  
+
   saucerManager.checkCollisions(asteroids);
   saucerManager.display();
   saucerManager.update();
-  
+
   asteroidManager.display();
   asteroidManager.update();
 }
@@ -160,16 +148,16 @@ function playingGameStateUpdate() {
 function displayScore() {
   let scoreText = "Score: " + ship.score;
   push();
-    textAlign(LEFT);
-    text(scoreText, 20, 30);
+  textAlign(LEFT);
+  text(scoreText, 20, 30);
   pop();
 }
 
 function displayHealth() {
   let scoreText = "Health: " + ship.health;
   push();
-    textAlign(RIGHT);
-    text(scoreText, width-20, 30);
+  textAlign(RIGHT);
+  text(scoreText, width - 20, 30);
   pop();
 }
 
@@ -189,7 +177,7 @@ function keyPressed() {
     // The space key is being pressed.
     ship.fire();
   }
-  
+
   if (keyIsDown(83)) {
     // The 's' key is being pressed.
     ship.teleport();
@@ -205,6 +193,35 @@ function keyPressed() {
     // The 'w' key is being pressed.
     ship.turnOnEngine(true);
   }
+}
+
+function newGame() {
+  let startingPosition = createVector(width / 2, height / 2);
+  let startingVelocity = createVector(0, 0);
+  let heading = 0;
+  let spriteSize = 20;
+  let health = 5;
+  ship = new Player(
+    startingPosition,
+    startingVelocity,
+    heading,
+    spriteSize,
+    playerSprite,
+    health
+  );
+
+  asteroidManager = new AsteroidManager(
+    numAsteroids,
+    largeSprite,
+    mediumSprite,
+    smallSprite
+  );
+
+  saucerManager = new SaucerManager(
+    enemySpriteLarge,
+    enemySpriteSmall,
+    smallSaucerSpawnProb
+  );
 }
 
 function keyReleased() {
